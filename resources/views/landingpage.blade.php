6a29bdf836426fc3c92a5bcb01ff80d0018cf53c
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-10-13 08:14:53 -0400

    new commit

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -11,7 +11,7 @@
     <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
-           <div class="flex">
+           <div class="flex ">
             <div class="shrink-0 flex items-center">
                 {{-- <a >
                     <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
@@ -34,6 +34,11 @@
                 <x-nav-link>
                     {{ __('About Us') }}
                 </x-nav-link> 
+            </div>
+            <div class="flex justify-between h-16 ml-20">
+                <x-nav-link>
+                    {{ __('Contact Us') }}
+                </x-nav-link>
             </div>
            </div>
 
