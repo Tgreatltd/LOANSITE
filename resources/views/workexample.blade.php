031abb68e1ff33f0548628987eaec7f5cbbfc939
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-10-13 07:45:01 -0400

    new commit

--- resources/views/workexample.blade.php
+++ resources/views/workexample.blade.php
@@ -12,11 +12,6 @@
      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
            <div class="flex">
-            <div class="shrink-0 flex items-center">
-                <a >
-                    <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
-                </a>
-            </div>
             <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                 <x-nav-link>
                     <h3 class=" m-20">OLUWATOBI</h3> 
