41b7844385128a8e0d4503efae4bbd23aa5ecc81
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-10-13 08:29:33 -0400

    new commit

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -13,7 +13,7 @@
             <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                <div class="flex justify-between h-16">
                   <div class="flex ">
-                   <div class="shrink-0 flex items-center">
+                   <div class="shrink-0 flex items-center text-green-600 text-3xl m-2">
                        {{-- <a >
                            <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                        </a> --}}
