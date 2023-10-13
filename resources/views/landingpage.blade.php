4a92619f66d73c582daf50e916e2bdebe412c1b8
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-10-13 06:17:47 -0400

    new commit

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -9,8 +9,10 @@
 <body>
     <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div>
+        <div class="flex justify-between h-16">
+           <div class="flex">
 
+           </div>
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <x-dropdown align="right" width="48">
