144670c6f4a9876d7819d5f18aaeeb5db26586e3
oluwatobi <olamoyegunoluwagreat@gmail.com>
2023-10-13 07:01:37 -0400

    new commit

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -1,37 +1,45 @@
-<x-app-layout>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
     <nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
-        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-           <div class="flex justify-between h-16">
-              <div class="flex">
-                <div>tobi</div>
-                <div>tobi</div>
-                <div>tobi</div>
-              </div>
-   
-               <div class="hidden sm:flex sm:items-center sm:ml-6">
-                   <x-dropdown align="right" width="48">
-                         <x-slot name="trigger">
-                             <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                                 <div>OLUWATOBI</div>
-         
-                                 <div class="ml-1">
-                                     <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                         <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                     </svg>
-                                 </div>
-                             </button>
-                         </x-slot>
-         
-                         <x-slot name="content">
-                             <x-dropdown-link>
-                               name
-                             </x-dropdown-link>
-                         </x-slot>
-                   </x-dropdown>
-                 </div>
+     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex justify-between h-16">
+           <div class="flex">
+             <div>tobi</div>
+             <div>tobi</div>
+             <div>tobi</div>
            </div>
-       </div>   
-           
-      </nav>
-</x-app-layout>
+
+            <div class="hidden sm:flex sm:items-center sm:ml-6">
+                <x-dropdown align="right" width="48">
+                      <x-slot name="trigger">
+                          <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                              <div>OLUWATOBI</div>
+      
+                              <div class="ml-1">
+                                  <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                      <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                  </svg>
+                              </div>
+                          </button>
+                      </x-slot>
+      
+                      <x-slot name="content">
+                          <div>Name</div>
+                      </x-slot>
+                </x-dropdown>
+              </div>
+        </div>
+    </div>   
+        
+   </nav>
+</body>
+</html>
